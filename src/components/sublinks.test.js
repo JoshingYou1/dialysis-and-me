443bd4d932bf1a151625af09c2a75efd7d2cbcb2
Joshua Drumm <joshing_you1@yahoo.com>
2019-04-15 23:09:04 -0400

    Trying to fix an issue with production app

--- src/components/sublinks.test.js
+++ src/components/sublinks.test.js
@@ -1,2 +1,19 @@
 import React from 'react';
 import {shallow} from 'enzyme';
+import {BrowserRouter as Router, Link} from 'react-router-dom';
+
+import {Sublinks} from './sublinks';
+
+describe('<Sublinks />', () => {
+    it('Should render without crashing', () => {
+        shallow(<Sublinks />);
+    });
+
+    it('Should render the Link component', () => {
+        shallow(
+            <Router>
+                <Link to=""/>
+            </Router>
+        );
+    });
+});
--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
 'use strict';
 
+export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
 // export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:8080';
-export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'https://nephrology-nexus-portal.herokuapp.com/';
+// export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'https://nephrology-nexus-portal.herokuapp.com/';
